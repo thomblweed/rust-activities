5cbf1fdd46aa62a3a227d91a31911c2620c5b3bc
Thomas Newman <thomblweed@gmail.com>
2022-12-14 08:18:09 +0000

    vectors exercise

--- src/activities/src/bin/a13.rs
+++ src/activities/src/bin/a13.rs
@@ -5,9 +5,27 @@
 // * Print the total number of elements in a vector
 //
 // Notes:
-// * Use a vector to store 4 numbers
-// * Iterate through the vector using a for..in loop
-// * Determine whether to print the number or print "thirty" inside the loop
-// * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let numbers = vec![10, 20, 30, 40];
+    // assigning length of the vector to a variable so that we don't need to transfer numbers ownership to the for loop
+    let numbers_length = numbers.len();
+
+    // * Iterate through the vector using a for..in loop
+    for number in numbers {
+        // * Determine whether to print the number or print "thirty" inside the loop
+        // if number == 30 {
+        //     println!("thirty");
+        //     break;
+        // }
+        // println!("{}", number);
+        // alternative using match
+        match number {
+            30 => println!("thirty"),
+            _ => println!("{:?}", number),
+        }
+    }
+    // * Use the .len() function to print the number of elements in a vector
+    println!("{}", numbers_length);
+}
