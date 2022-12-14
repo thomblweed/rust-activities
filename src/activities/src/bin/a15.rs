8b74ad16f4ef30dec65018dcab410eb56c8ca94b
Thomas Newman <thomblweed@gmail.com>
2022-12-14 21:26:28 +0000

    advanced match activity

--- src/activities/src/bin/a15.rs
+++ src/activities/src/bin/a15.rs
@@ -7,8 +7,31 @@
 // * All tickets include the price
 //
 // Notes:
-// * Use an enum for the tickets with data associated with each variant
-// * Create one of each ticket and place into a vector
-// * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+fn main() {
+    // * Use an enum for the tickets with data associated with each variant
+    #[derive(Debug)]
+    enum Ticket {
+        Backstage(String, f64),
+        VIP(String, f64),
+        Standard(f64),
+    }
+    // * Create one of each ticket and place into a vector
+    let tickets: Vec<Ticket> = vec![
+        Ticket::Backstage("Thomas".to_owned(), 5.55),
+        Ticket::VIP("Dave".to_owned(), 10.99),
+        Ticket::Standard(2.55),
+    ];
+    // * Use a match expression while iterating the vector to print the ticket info
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(name, price) => {
+                println!("Backstage, Price: ${:?}, Name: {:?}", price, name)
+            }
+            Ticket::VIP(name, price) => {
+                println!("VIP, Price: ${:?}, Name: {:?}", price, name)
+            }
+            Ticket::Standard(price) => println!("Standard, Price: ${:?}", price),
+        }
+    }
+}
