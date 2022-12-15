5b679af2a18769244d59c9539e1106971f1a8878
Thomas Newman <thomblweed@gmail.com>
2022-12-15 12:33:32 +0000

    exercise for optinal

--- src/activities/src/bin/a16.rs
+++ src/activities/src/bin/a16.rs
@@ -5,10 +5,37 @@
 // * Lockers use numbers and are optional for students
 //
 // Notes:
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+fn main() {
+    // * Use a struct containing the student's name and locker assignment
+    // * The locker assignment should use an Option<i32>
+    struct Student {
+        name: String,
+        locker_assignment: Option<i32>,
+    }
 
-fn main() {}
+    impl Student {
+        fn new(name: String, locker_assignment: Option<i32>) -> Self {
+            Self {
+                name,
+                locker_assignment,
+            }
+        }
+        fn print(self) {
+            println!("Name: {:?}", self.name);
+            match self.locker_assignment {
+                Some(locker_number) => println!("Locker: {:?}", locker_number),
+                None => println!("No locker assigned!"),
+            }
+        }
+    }
+
+    let students = vec![
+        Student::new("thom".to_owned(), None),
+        Student::new("jeff".to_owned(), Some(23)),
+    ];
+
+    for student in students {
+        student.print();
+    }
+}
